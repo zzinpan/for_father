27e32383c7a82619b6f7a2563b88aee6b6b98dd3
12y8e <12y8e@DESKTOP-RDDTRBB>
2020-04-08 00:01:39 +0900

    정산 퍼블리싱 진행중..

---
+++ controller/CalculationController.js
@@ -0,0 +1,63 @@
+module.exports = [
+	
+	/**
+	 * 품목관리 화면
+	 */
+	{
+		url: "/calculation/management/view",
+		type: "get",
+		method: function( req, res, next ){
+			
+			fs.readFile(
+				global.dataDir + "/product.json",
+				"utf8",
+				function( err, products ){ 
+					
+					products = JSON.parse( products );
+					products.sort(function( a, b ){
+						
+						if( a.order < b.order ){
+							return -1;
+						}
+						if( a.order > b.order ){
+							return 1;
+						}
+						
+						return 0;
+						
+					});
+					
+					res.render( "WEB-INF/calculation/management.ejs", {
+						products: JSON.stringify( products )
+					} );
+				}
+			);
+				
+		}
+	},
+	
+	/**
+	 * 품목 저장
+	 */
+	{
+		url: "/calculation/management/save",
+		type: "post",
+		method: function( req, res, next ){
+
+			var groups = req.body;
+			fs.writeFile( 
+				global.dataDir + "/product.json", 
+				JSON.stringify( groups ),
+				"utf8",
+				function(err){ 
+					if (err == null) { 
+						res.send( { result: "SUCCESS", data: null } );
+					} else { 
+						res.send( { result: "FAIL", data: err } );
+					} 
+			});
+			
+		}
+	},
+	
+];
